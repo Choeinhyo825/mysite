ea8a2a87cf9e37250aa3825419671651f5e084ff
inhyo <inhyo825@gmail.com>
2020-02-26 10:28:20 +0900

    MyBatis guestBook 적용

--- mysite03/src/main/java/com/douzone/mysite/repository/GuestbookRepository.java
+++ mysite03/src/main/java/com/douzone/mysite/repository/GuestbookRepository.java
@@ -1,133 +1,31 @@
 package com.douzone.mysite.repository;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.sql.DataSource;
-
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.douzone.mysite.exception.GuestBookRepositoryException;
 import com.douzone.mysite.vo.GuestbookVo;
 
 @Repository
 public class GuestbookRepository {
 
 	@Autowired
-	private DataSource dataSource;
+	private SqlSession sqlSession;
+	
+	public List<GuestbookVo> findAll() {
+		return sqlSession.selectList("guestbook.findAll");
+	}
 
 	public int insert(GuestbookVo vo) {
-		int count = 0;
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-
-		try {
-			conn = dataSource.getConnection();
-
-			String sql = "insert into guestbook values(null, ?, ?, ?, now())";
-			pstmt = conn.prepareStatement(sql);
-
-			pstmt.setString(1, vo.getName());
-			pstmt.setString(2, vo.getContents());
-			pstmt.setString(3, vo.getPass());
-
-			count = pstmt.executeUpdate();
-
-		} catch (SQLException e) {
-			throw new GuestBookRepositoryException(e.getMessage());
-		} finally {
-			try {
-				if (pstmt != null)
-					pstmt.close();
-				if (conn != null)
-					conn.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		return count;
+		return sqlSession.insert("guestbook.insert", vo);
 	}
 
-	public int delete(Long no, String pass) {
-		int count = 0;
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-
-		try {
-			conn = dataSource.getConnection();
-
-			String sql = "delete from guestbook where no=? and pass=?";
-			pstmt = conn.prepareStatement(sql);
-
-			pstmt.setLong(1, no);
-			pstmt.setString(2, pass);
-
-			count = pstmt.executeUpdate();
-
-		} catch (SQLException e) {
-			throw new GuestBookRepositoryException(e.getMessage());
-		} finally {
-			try {
-				if (pstmt != null)
-					pstmt.close();
-				if (conn != null)
-					conn.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		return count;
+	public int delete(GuestbookVo vo) {
+		return sqlSession.delete("guestbook.delete", vo);
 	}
 
-	public List<GuestbookVo> findAll() {
-		List<GuestbookVo> list = new ArrayList<GuestbookVo>();
 
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rset = null;
-
-		try {
-			conn = dataSource.getConnection();
-			String sql = "select nodataSource.getConnection();, reg_date from guestbook order by 1 desc";
-			pstmt = conn.prepareStatement(sql);
-			rset = pstmt.executeQuery();
-
-			while (rset.next()) {
-				Long no = rset.getLong(1);
-				String name = rset.getString(2);
-				String contents = rset.getString(3);
-				String regDate = rset.getString(4);
-
-				GuestbookVo vo = new GuestbookVo();
-
-				vo.setNo(no);
-				vo.setName(name);
-				vo.setContents(contents);
-				vo.setRegDate(regDate);
-				list.add(vo);
-			}
-
-		} catch (SQLException e) {
-			throw new GuestBookRepositoryException(e.getMessage());
-		} finally {
-			try {
-				if (rset != null)
-					rset.close();
-				if (pstmt != null)
-					pstmt.close();
-				if (conn != null)
-					conn.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		return list;
-	}
 
 }
