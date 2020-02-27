911428dd715123455b6b8a2111c7e49d9ba8a7dd
R35 <R35@DESKTOP-ITE4USS>
2020-02-27 16:44:26 +0900

    Admin interceptor / 접근제어 구현

--- mysite03/src/main/java/com/douzone/mysite/controller/UserContoller.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/UserContoller.java
@@ -1,33 +1,32 @@
 package com.douzone.mysite.controller;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.douzone.mysite.service.UserService;
 import com.douzone.mysite.vo.UserVo;
+import com.douzone.security.Auth;
+import com.douzone.security.AuthUser;
 
 @Controller
 @RequestMapping("/user")
 public class UserContoller {
-	
+
 	@Autowired
 	private UserService userService;
 
-	/* 
+	/*
 	 * 회원가입 폼
 	 */
 	@RequestMapping(value = "/join", method = RequestMethod.GET)
 	public String join() {
 		return "user/join";
 	}
-	/* 
+
+	/*
 	 * 회원가입
 	 */
 	@RequestMapping(value = "/join", method = RequestMethod.POST)
@@ -35,83 +34,45 @@ public class UserContoller {
 		userService.join(vo);
 		return "redirect:/user/joinsuccess";
 	}
-	/* 
+
+	/*
 	 * 회원가입 성공 폼
 	 */
 	@RequestMapping("/joinsuccess")
 	public String joinsuccess() {
 		return "user/joinsuccess";
 	}
-	/* 
+
+	/*
 	 * 로그인 폼
 	 */
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String login() {
 		return "user/login";
 	}
-	/* 
-	 * 로그인
-	 */
-	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public String login(HttpSession session, @ModelAttribute UserVo vo) {
-		UserVo authUser = userService.getUser(vo);
-		if(authUser == null) {
-			return "user/login";
-		}
-		session.setAttribute("authUser", authUser);
-		return "redirect:/";
-	}
-	/* 
-	 * 로그아웃
-	 */
-	@RequestMapping("/logout")
-	public String logout(HttpSession session) {
-		
-		//////////////////////////// 접근제어 ///////////////////////////
-		UserVo authUser = (UserVo)session.getAttribute("authUser");
-		if(authUser == null) {
-			return "redirect:/";
-		}
-		//////////////////////////////////////////////////////////////
-		
-		session.removeAttribute("authUser");
-		session.invalidate();
-		return "redirect:/";
-	}
-	/* 
+
+	/*
 	 * 회원정보 수정 폼
 	 */
+	@Auth
 	@RequestMapping(value = "/update", method = RequestMethod.GET)
-	public String update(HttpSession session, Model model) {
-		
-		//////////////////////////// 접근제어 ///////////////////////////
-		UserVo authUser = (UserVo)session.getAttribute("authUser");
-		if(authUser == null) {
-			return "redirect:/";
-		}
-		//////////////////////////////////////////////////////////////
-		
+	public String update(Model model, @AuthUser UserVo authUser) {
 		UserVo vo = userService.getUser(authUser.getNo());
-		model.addAttribute("userVo",vo);
+		model.addAttribute("userVo", vo);
 		return "/user/update";
 	}
-	/* 
+
+	/*
 	 * 회원정보 수정
 	 */
+	@Auth
 	@RequestMapping(value = "/update", method = RequestMethod.POST)
-	public String update(HttpSession session, UserVo vo) {
-		
-		//////////////////////////// 접근제어 ///////////////////////////
-		UserVo authUser = (UserVo)session.getAttribute("authUser");
-		if(authUser == null) {
-			return "redirect:/";
-		}
-		//////////////////////////////////////////////////////////////
-		
-		 userService.updateUser(vo);
+	public String update(UserVo vo, @AuthUser UserVo authUser) {
+		vo.setNo(authUser.getNo());
+		userService.updateUser(vo);
+		authUser.setName(vo.getName());
 		return "/user/updatesuccess";
 	}
-	
 //	@ExceptionHandler(Exception.class)
 //	public String handleException() {
 //		return "error/exception";
