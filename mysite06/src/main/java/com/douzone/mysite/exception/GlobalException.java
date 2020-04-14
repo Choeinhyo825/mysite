4a9834b6d47db3a1fd11ec138ff604a88a2cf14e
inhyo <inhyo825@gmail.com>
2020-04-14 15:04:25 +0900

    mysite06 - message converter

--- mysite06/src/main/java/com/douzone/mysite/exception/GlobalException.java
+++ mysite06/src/main/java/com/douzone/mysite/exception/GlobalException.java
@@ -1,5 +1,6 @@
 package com.douzone.mysite.exception;
 
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
@@ -11,9 +12,12 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.douzone.mysite.dto.JsonResult;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @ControllerAdvice
 public class GlobalException {
-	
+
 	private static final Log LOG = LogFactory.getLog(GlobalException.class);
 
 	@ExceptionHandler(Exception.class)
@@ -24,13 +28,29 @@ public class GlobalException {
 		StringWriter errors = new StringWriter(); // 버퍼
 		e.printStackTrace(new PrintWriter(errors));
 		e.printStackTrace();
+		LOG.error(errors.toString());
 
-		 LOG.error(errors.toString());
+		// 2. 요청 구분
+		// 민약, JSON 요청인 경우에는 request Header의 Accpet에 Application/json
+		// 민약, HTML 요청인 경우에는 request Header의 Accpet에 text/html
+		// 민약, jpeg 요청인 경우에는 request Header의 Accpet에 image/jpeg
+		String accept = request.getHeader("accept");
 
-		// 2. 안내 페이지 가기(정상종료)
-		request.setAttribute("exception", errors.toString());
-		request.getRequestDispatcher("/WEB-INF/views/error/exception.jsp").forward(request, response);
-
+		if (accept.matches(".*application/json.*")) {
+			// 3.JSON 응답
+			response.setStatus(HttpServletResponse.SC_OK);
+			
+			JsonResult jsonResult = JsonResult.fail(errors.toString());
+			String jsonString = new ObjectMapper().writeValueAsString(jsonResult);
+			
+			OutputStream os = response.getOutputStream();
+			os.write(jsonString.getBytes("utf-8"));
+			os.close();
+			
+		} else {
+			// 3. 안내페이지 가기(정상종료)
+			request.setAttribute("exception", errors.toString());
+			request.getRequestDispatcher("/WEB-INF/views/error/exception.jsp").forward(request, response);
+		}
 	}
-
 }
